6e908bd4f5572ce1d7dbaab1633d306d45f66dec
Nordwolf1968 <fachinformatik@yahoo.com>
2023-07-17 10:40:22 +0200

    parametrisierung

--- script_01.js
+++ script_01.js
@@ -8,12 +8,23 @@
 // Funktionsrumpf (bosy); callee
 // Funktionsdeklaration
 
-function test()
-{
-    console.log("Hallo Ingo");
-
-}
+// function test()
+// {
+//    console.log("Hallo Ingo");
+// }
 
 // Funktionsaufruf (call)
-test();
+// test();
 
+/***** Funktionen 02a  *****/
+// 2a Parametrisierung + Datenübergabe von INNEN
+
+ausgabeNamen();
+
+function ausgabeNamen() {
+    
+    let firstName = "Corinna";
+console.log("Hallo " + firstName + "!");
+}
+
+// console.log(firstName);     // Fehler --> SCOPE! ausserhalb der function ist die variable nicht zugängig
